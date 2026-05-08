4295776122e2ac2abaf0cb57a76eaaaa702ab3e1
Tommaso Moretti <tommasomoretti88@gmail.com>
2026-05-08 08:32:13 +0200

    feat: add view_cart event to e-commerce funnel logic and session steps

--- tables/sql/ec_closed_funnel.sql
+++ tables/sql/ec_closed_funnel.sql
@@ -17,13 +17,14 @@ with session_steps as (
 
       MIN(IF(event_name = 'view_item', event_timestamp, NULL)) AS view_item_ts,
       MIN(IF(event_name = 'add_to_cart', event_timestamp, NULL)) AS add_to_cart_ts,
+      MIN(IF(event_name = 'view_cart', event_timestamp, NULL)) AS view_cart_ts,
       MIN(IF(event_name = 'begin_checkout', event_timestamp, NULL)) AS begin_checkout_ts,
       MIN(IF(event_name = 'add_shipping_info', event_timestamp, NULL)) AS add_shipping_info_ts,
       MIN(IF(event_name = 'add_payment_info', event_timestamp, NULL)) AS add_payment_info_ts,
       MIN(IF(event_name = 'purchase', event_timestamp, NULL)) AS purchase_ts
   
     from `tom-moretti.nameless_analytics.events`(start_date, end_date, 'session')
-    where event_name in ('view_item', 'add_to_cart', 'begin_checkout', 'add_shipping_info', 'add_payment_info', 'purchase')
+    where event_name in ('view_item', 'add_to_cart', 'view_cart', 'begin_checkout', 'add_shipping_info', 'add_payment_info', 'purchase')
     group by all
   )
   
@@ -43,9 +44,10 @@ with session_steps as (
     case when session_start_timestamp is not null then client_id end as session_start,
     case when view_item_ts > session_start_timestamp then client_id end as view_item,
     case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts then client_id end as add_to_cart,
-    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and begin_checkout_ts > add_to_cart_ts then client_id end as begin_checkout,
-    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and begin_checkout_ts > add_to_cart_ts and add_shipping_info_ts > begin_checkout_ts then client_id end as add_shipping_info,
-    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and begin_checkout_ts > add_to_cart_ts and add_shipping_info_ts > begin_checkout_ts and add_payment_info_ts > add_shipping_info_ts then client_id end as add_payment_info,
-    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and begin_checkout_ts > add_to_cart_ts and add_shipping_info_ts > begin_checkout_ts and add_payment_info_ts > add_shipping_info_ts and purchase_ts > add_payment_info_ts then client_id end as purchase
+    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and view_cart_ts > add_to_cart_ts then client_id end as view_cart,
+    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and view_cart_ts > add_to_cart_ts and begin_checkout_ts > add_to_cart_ts then client_id end as begin_checkout,
+    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and view_cart_ts > add_to_cart_ts and begin_checkout_ts > add_to_cart_ts and add_shipping_info_ts > begin_checkout_ts then client_id end as add_shipping_info,
+    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and view_cart_ts > add_to_cart_ts and begin_checkout_ts > add_to_cart_ts and add_shipping_info_ts > begin_checkout_ts and add_payment_info_ts > add_shipping_info_ts then client_id end as add_payment_info,
+    case when view_item_ts > session_start_timestamp and add_to_cart_ts > view_item_ts and view_cart_ts > add_to_cart_ts and begin_checkout_ts > add_to_cart_ts and add_shipping_info_ts > begin_checkout_ts and add_payment_info_ts > add_shipping_info_ts and purchase_ts > add_payment_info_ts then client_id end as purchase
   from session_steps
 );
