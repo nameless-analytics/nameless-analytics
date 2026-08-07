de9f9e992d4a3fa29653fb26e4ac82a1224e013a
Tommaso Moretti <tommasomoretti88@gmail.com>
2026-08-08 00:15:55 +0200

    feat: add attribution comparison table function and enhance multi-touch logic with non-direct lookbacks

---
+++ tables/table-functions/attribution_comparison.sql
@@ -0,0 +1,58 @@
+CREATE OR REPLACE TABLE FUNCTION `tom-moretti.nameless_analytics.attribution_comparison`(start_date DATE, end_date DATE, conversion_event STRING, lookback_days INT64) AS (
+  with attribution_data_single_touch as (
+    select *
+    from `tom-moretti.nameless_analytics.attribution_single_touch`(start_date, end_date, conversion_event, lookback_days)
+  ),
+
+  attribution_data_multi_touch as (
+    select *
+    from `tom-moretti.nameless_analytics.attribution_multi_touch`(start_date, end_date, conversion_event, lookback_days)
+  ),
+
+  attribution_models as (
+    # LAST CLICK
+    select
+        last_click_channel_grouping as channel_grouping,
+        last_click_source as source,
+        conversion_id,
+        'last_click' as attribution_model
+    from attribution_data_single_touch
+  
+    union all
+  
+    # FIRST CLICK
+    select
+        first_click_channel_grouping as channel_grouping,
+        first_click_source as source,
+        conversion_id,
+        'first_click' as attribution_model
+    from attribution_data_single_touch
+  
+    union all
+  
+    # LAST CLICK NON-DIRECT
+    select
+        last_click_non_direct_channel_grouping as channel_grouping,
+        last_click_non_direct_source as source,
+        conversion_id,
+        'last_click_non_direct' as attribution_model
+    from attribution_data_single_touch
+
+    # LINEAR
+    # TIME DECAY
+    # POSITION-BASED
+
+  )
+
+  select
+    channel_grouping,
+    source,
+
+    countif(attribution_model = 'last_click') as conversions_last_click,
+    countif(attribution_model = 'first_click') as conversions_first_click,
+    countif(attribution_model = 'last_click_non_direct') as conversions_last_click_non_direct
+
+  from attribution_models
+  group by 1, 2
+  order by conversions_last_click desc
+)
