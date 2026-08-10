84acec3b8b6fa82bfcadbe1df3fa85bb44269065
Tommaso Moretti <tommasomoretti88@gmail.com>
2026-08-10 13:13:28 +0200

    refactor: update date formatting to ISO standard and revise budget tracking metrics in media_plan table function

--- tables/table-functions/media_plan.sql
+++ tables/table-functions/media_plan.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE FUNCTION `tom-moretti.nameless_analytics.media_plan`(start_date DATE, end_date DATE) AS (
 with media_plan_data as (
     SELECT
-      FORMAT_DATE('%m-%Y', date) as date,
+      FORMAT_DATE('%Y-%m', date) as date,
       full_campaign_name,
       budget
     FROM `tom-moretti.nameless_analytics.media_plan_sheets`
@@ -12,10 +12,10 @@ with media_plan_data as (
 
   online_campaigns_performances as (
     select
-      FORMAT_DATE('%m-%Y', date) AS date,
+      FORMAT_DATE('%Y-%m', date) AS date,
       campaign_name,
       campaign_id,
-      cost as cost
+      cost as spend
     from `tom-moretti.nameless_analytics.online_campaign_performance_sheets`
     where true
       and date between start_date and end_date
@@ -24,7 +24,7 @@ with media_plan_data as (
   )
 
   select 
-    media_plan_data.date,
+    media_plan_data.date as year_month,
     media_plan_data.full_campaign_name,
     split(media_plan_data.full_campaign_name, '|')[safe_offset(0)] as campaign_year,
     split(media_plan_data.full_campaign_name, '|')[safe_offset(1)] as campaign_country,
@@ -34,8 +34,9 @@ with media_plan_data as (
     split(media_plan_data.full_campaign_name, '|')[safe_offset(5)] as marketing_objective,
     split(media_plan_data.full_campaign_name, '|')[safe_offset(6)] as campaign_name,
     media_plan_data.budget,
-    online_campaigns_performances.cost,
-    ifnull(1 - safe_divide((media_plan_data.budget - online_campaigns_performances.cost), media_plan_data.budget), 0.00) as spent_over_budget_percentage
+    online_campaigns_performances.spend,
+    IFNULL(SAFE_DIVIDE(media_plan_data.budget - online_campaigns_performances.spend, media_plan_data.budget), 0.00) AS remaining_budget_percentage,
+    IFNULL(SAFE_DIVIDE(online_campaigns_performances.spend, media_plan_data.budget), 0.00) AS spent_budget_percentage
   from media_plan_data
   left join online_campaigns_performances
     on media_plan_data.date = online_campaigns_performances.date
