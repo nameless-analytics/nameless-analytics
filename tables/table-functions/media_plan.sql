d2492c5db34ba9bf53b074ed75e202379fcff051
Tommaso Moretti <tommasomoretti88@gmail.com>
2026-08-20 19:24:25 +0200

    refactor: aggregate media plan budget by campaign and ensure null-safe spend calculation

--- tables/table-functions/media_plan.sql
+++ tables/table-functions/media_plan.sql
@@ -16,16 +16,17 @@ declare dataset_name string default 'nameless_analytics';
 declare media_plan string default format ("""
 CREATE OR REPLACE TABLE FUNCTION `%s.%s.media_plan`(start_date DATE, end_date DATE) AS (
 with media_plan_data as (
-    SELECT
-      FORMAT_DATE('%%Y-%%m', date) as year_month,
-      FORMAT_DATE('%%Y', date) as year,
-      FORMAT_DATE('%%m', date) as month,
+    select
+      format_date('%%Y-%%m', date) as year_month,
+      format_date('%%Y', date) as year,
+      format_date('%%m', date) as month,
       full_campaign_name as campaign,
-      budget
-    FROM `%s.%s.media_plan_sheets`
+      ifnull(sum(budget), 0.0) as budget
+    from `%s.%s.media_plan_sheets`
     where true
       and date between start_date and end_date
       and full_campaign_name is not null
+    group by all
   ),
 
   online_campaigns_performances as (
@@ -52,13 +53,12 @@ with media_plan_data as (
     `%s.%s.get_campaign_part`(media_plan_data.campaign, 'campaign_marketing_objective') as campaign_marketing_objective,
     `%s.%s.get_campaign_part`(media_plan_data.campaign, 'campaign_name') as campaign_name,
     media_plan_data.campaign,
-    sum(media_plan_data.budget) as budget,
-    sum(online_campaigns_performances.spend) as spend
+    media_plan_data.budget,
+    ifnull(online_campaigns_performances.spend, 0.0) as spend
   from media_plan_data
   left join online_campaigns_performances
     on media_plan_data.year_month = online_campaigns_performances.year_month
     and media_plan_data.campaign = online_campaigns_performances.campaign
-  group by all
 );
 """,
 project_name, dataset_name,
